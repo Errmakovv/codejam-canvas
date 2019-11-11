510714ae8f79420012f7a1412cf2ec75f518088d
Timur Yermakou <darkday3@mail.ru>
2019-11-11 20:16:40 +0300

    refactor: change let to const

--- codejam-canvas/data.js
+++ codejam-canvas/data.js
@@ -1,11 +1,11 @@
-export let a = [
+export const a = [
     ["00BCD4", "FFEB3B", "FFEB3B", "00BCD4"],
     ["FFEB3B", "FFC107", "FFC107", "FFEB3B"],
     ["FFEB3B", "FFC107", "FFC107", "FFEB3B"],
     ["00BCD4", "FFEB3B", "FFEB3B", "00BCD4"]
 ];
 
-export let b = [
+export const b = [
     [
         [
             255,
